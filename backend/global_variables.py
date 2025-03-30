aabe6b49d17f2bf7bab74ec025161d99752ba9c6
Jenslee Dsouza <dsouzajenslee@gmail.com>
2025-03-30 13:20:40 +0530

    Update CORS_URLS to include additional domain for API access

--- backend/global_variables.py
+++ backend/global_variables.py
@@ -1,6 +1,6 @@
 URL_PREFIX = "/snake-game"
 
 SQLALCHEMY_DATABASE_URI = "sqlite:////data/score.db"
-CORS_URLS = ["https://lockhart07.github.io"]
+CORS_URLS = ["https://lockhart07.github.io", "https://slitherbite.lockhart.in"]
 
 GET_SCORES_POLLING_RATE_SECONDS = 5
